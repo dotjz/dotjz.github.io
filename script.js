423c35a083628df71883a45a2fa880d09a880f9c
Mateo <17AbascalM@prepstudents.org>
2014-12-21 20:27:51 -0800

    maybe fix for search bar

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
 	$('input.search').on('click', function(){
 		$('input.search').animate({width: "300px"},500);
 	});
+	$('input.search').keyup(function(){
+         var page = $('p.egp');
+         var pageText = page.text().replace("<span class='search'>","").replace("</span>");
+         var searchedText = $('input.search').val();
+         var text = new RegExp("("+searchedText+")", "igm");    
+         var newHtml = pageText.replace(text ,"<span class='search'>$1</span>");
+         page.html(newHtml);
+    });
 });
